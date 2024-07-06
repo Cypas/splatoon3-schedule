90f86f779e44453185c1a1a3799280abeb3e8713
cypas <ayano05@outlook.com>
2024-07-06 12:17:26 +0800

    perf:更新帮助菜单内配装的介绍；更新nso菜单内/lasti指令介绍

--- test.py
+++ test.py
@@ -129,13 +129,23 @@ from nonebot_plugin_splatoon3_schedule.util import write_weapon_trans_dict
 # res = get_festival_image()
 # res.show()
 
+
 # 测试帮助
-# res = get_help_image()
-# res.show()
+# async def get_help():
+#     img = await get_help_image()
+#     img.show()
+#
+#
+# asyncio.run(get_help())
+
 
 # 测试nso帮助
-# res = get_nso_help_image()
-# res.show()
+# async def get_nso_help():
+#     img = await get_nso_help_image()
+#     img.show()
+#
+#
+# asyncio.run(get_nso_help())
 
 # 清空缓存
 # db_image.clean_image_temp()
