1a3a04f501733bf3e30dee19bb56f954c5b1afdc
qinyi <ayano05@outlook.com>
2025-11-04 16:04:09 +0800

    fix:qq无法发送图片的问题

--- nonebot_plugin_splatoon3_schedule/util.py
+++ nonebot_plugin_splatoon3_schedule/util.py
@@ -1,10 +1,18 @@
 import time
 
 # from nonebot.adapters.qq import AuditException, ActionFailed
-from nonebot.adapters.qq import AuditException as QQ_AuditException, ActionFailed as QQ_ActionFailed
+from nonebot.adapters.qq import (
+    AuditException as QQ_AuditException,
+    ActionFailed as QQ_ActionFailed,
+)
 
 from .config import plugin_config
-from .image.image import get_save_temp_image, get_stages_image, get_coop_stages_image, get_events_image
+from .image.image import (
+    get_save_temp_image,
+    get_stages_image,
+    get_coop_stages_image,
+    get_events_image,
+)
 from .utils.utils import get_time_now_china
 from .data import db_control, db_image
 from .utils.bot import *
@@ -106,16 +114,24 @@ async def send_msg(bot: Bot, event: Event, msg: str | bytes):
     if isinstance(msg, str):
         # 文字消息
         if isinstance(bot, V11_Bot):
-            await bot.send(event, message=V11_MsgSeg.text(msg), reply_message=reply_mode)
+            await bot.send(
+                event, message=V11_MsgSeg.text(msg), reply_message=reply_mode
+            )
         elif isinstance(bot, V12_Bot):
-            await bot.send(event, message=V12_MsgSeg.text(msg), reply_message=reply_mode)
+            await bot.send(
+                event, message=V12_MsgSeg.text(msg), reply_message=reply_mode
+            )
         elif isinstance(bot, Tg_Bot):
             if reply_mode:
-                await bot.send(event, msg, reply_to_message_id=event.dict().get("message_id"))
+                await bot.send(
+                    event, msg, reply_to_message_id=event.dict().get("message_id")
+                )
             else:
                 await bot.send(event, msg)
         elif isinstance(bot, Kook_Bot):
-            await bot.send(event, message=Kook_MsgSeg.text(msg), reply_sender=reply_mode)
+            await bot.send(
+                event, message=Kook_MsgSeg.text(msg), reply_sender=reply_mode
+            )
         elif isinstance(bot, QQ_Bot):
             try:
                 await bot.send(event, message=QQ_MsgSeg.text(msg))
@@ -130,7 +146,11 @@ async def send_msg(bot: Bot, event: Event, msg: str | bytes):
         img = msg
         if isinstance(bot, V11_Bot):
             try:
-                await bot.send(event, message=V11_MsgSeg.image(file=img, cache=False), reply_message=reply_mode)
+                await bot.send(
+                    event,
+                    message=V11_MsgSeg.image(file=img, cache=False),
+                    reply_message=reply_mode,
+                )
             except Exception as e:
                 logger.warning(f"QQBot send error: {e}")
         elif isinstance(bot, V12_Bot):
@@ -139,16 +159,25 @@ async def send_msg(bot: Bot, event: Event, msg: str | bytes):
                 resp = await bot.upload_file(type="data", name="temp.png", data=img)
                 file_id = resp["file_id"]
                 if file_id:
-                    await bot.send(event, message=V12_MsgSeg.image(file_id=file_id), reply_message=reply_mode)
+                    await bot.send(
+                        event,
+                        message=V12_MsgSeg.image(file_id=file_id),
+                        reply_message=reply_mode,
+                    )
             except Exception as e:
                 logger.warning(f"QQBot send error: {e}")
         elif isinstance(bot, Tg_Bot):
             if reply_mode:
-                await bot.send(event, Tg_File.photo(img), reply_to_message_id=event.dict().get("message_id"))
+                await bot.send(
+                    event,
+                    Tg_File.photo(img),
+                    reply_to_message_id=event.dict().get("message_id"),
+                )
             else:
                 await bot.send(event, Tg_File.photo(img))
         elif isinstance(bot, Kook_Bot):
             url = await bot.upload_file(img)
+            logger.info("url:" + url)
             await bot.send(event, Kook_MsgSeg.image(url), reply_sender=reply_mode)
         elif isinstance(bot, QQ_Bot):
             if not isinstance(event, GroupAtMessageCreateEvent):
@@ -161,23 +190,35 @@ async def send_msg(bot: Bot, event: Event, msg: str | bytes):
                         logger.warning(f"QQ send msg error: {e}")
             else:
                 # 目前q群只支持url图片，得想办法上传图片获取url
-                kook_bot = None
-                bots = nonebot.get_bots()
-                for k, b in bots.items():
-                    if isinstance(b, Kook_Bot):
-                        kook_bot = b
-                        break
-                if kook_bot is not None:
-                    # 使用kook的接口传图片
-                    url = await kook_bot.upload_file(img)
-                    # logger.info("url:" + url)
-                    try:
-                        await bot.send(event, message=QQ_MsgSeg.image(url))
-                    except QQ_ActionFailed as e:
-                        if "消息被去重" in str(e):
-                            pass
-                        else:
-                            logger.warning(f"QQ send msg error: {e}")
+                url = await get_image_url(img)
+                logger.info("url:" + url)
+                try:
+                    await bot.send(event, message=QQ_MsgSeg.image(url))
+                except QQ_ActionFailed as e:
+                    if "消息被去重" in str(e):
+                        pass
+                    else:
+                        logger.warning(f"QQ send msg error: {e}")
+
+
+async def get_image_url(img: bytes) -> str:
+    """通过kook获取图片url"""
+    kook_bot = None
+    bots = nonebot.get_bots()
+    for k, b in bots.items():
+        if isinstance(b, Kook_Bot):
+            kook_bot = b
+            break
+    url = ""
+    if kook_bot is not None:
+        # 使用kook的接口传图片
+        url = await kook_bot.upload_file(img)
+        if url:
+            channel_id = plugin_config.splatoon3_kk_channel_waste_chat_id
+            await kook_bot.send_channel_msg(
+                channel_id=channel_id, message=Kook_MsgSeg.image(url)
+            )
+    return url
 
 
 async def send_channel_msg(bot: Bot, source_id, msg: str | bytes):
@@ -185,10 +226,14 @@ async def send_channel_msg(bot: Bot, source_id, msg: str | bytes):
     if isinstance(msg, str):
         # 文字消息
         if isinstance(bot, Kook_Bot):
-            await bot.send_channel_msg(channel_id=source_id, message=Kook_MsgSeg.text(msg))
+            await bot.send_channel_msg(
+                channel_id=source_id, message=Kook_MsgSeg.text(msg)
+            )
         elif isinstance(bot, QQ_Bot):
             try:
-                await bot.send_to_channel(channel_id=source_id, message=QQ_MsgSeg.text(msg))
+                await bot.send_to_channel(
+                    channel_id=source_id, message=QQ_MsgSeg.text(msg)
+                )
             except QQ_AuditException as e:
                 logger.warning(f"主动消息审核结果为{e.__dict__}")
             except QQ_ActionFailed as e:
@@ -200,10 +245,14 @@ async def send_channel_msg(bot: Bot, source_id, msg: str | bytes):
         img = msg
         if isinstance(bot, Kook_Bot):
             url = await bot.upload_file(img)
-            await bot.send_channel_msg(channel_id=source_id, message=Kook_MsgSeg.image(url))
+            await bot.send_channel_msg(
+                channel_id=source_id, message=Kook_MsgSeg.image(url)
+            )
         elif isinstance(bot, QQ_Bot):
             try:
-                await bot.send_to_channel(channel_id=source_id, message=QQ_MsgSeg.file_image(img))
+                await bot.send_to_channel(
+                    channel_id=source_id, message=QQ_MsgSeg.file_image(img)
+                )
             except QQ_AuditException as e:
                 logger.warning(f"主动消息审核结果为{e.__dict__}")
             except QQ_ActionFailed as e:
@@ -221,7 +270,9 @@ async def send_private_msg(bot: Bot, source_id, msg: str | bytes, event=None):
         elif isinstance(bot, QQ_Bot):
             try:
                 if event:
-                    await bot.send_to_dms(guild_id=event.guild_id, message=msg, msg_id=event.id)
+                    await bot.send_to_dms(
+                        guild_id=event.guild_id, message=msg, msg_id=event.id
+                    )
             except QQ_AuditException as e:
                 logger.warning(f"主动消息审核结果为{e.__dict__}")
             except QQ_ActionFailed as e:
@@ -234,10 +285,16 @@ async def send_private_msg(bot: Bot, source_id, msg: str | bytes, event=None):
         img = msg
         if isinstance(bot, Kook_Bot):
             url = await bot.upload_file(img)
-            await bot.send_private_msg(user_id=source_id, message=Kook_MsgSeg.image(url))
+            await bot.send_private_msg(
+                user_id=source_id, message=Kook_MsgSeg.image(url)
+            )
         elif isinstance(bot, QQ_Bot):
             try:
-                await bot.send_to_dms(guild_id=event.guild_id, message=QQ_MsgSeg.file_image(img), msg_id=event.id)
+                await bot.send_to_dms(
+                    guild_id=event.guild_id,
+                    message=QQ_MsgSeg.file_image(img),
+                    msg_id=event.id,
+                )
             except QQ_AuditException as e:
                 logger.warning(f"主动消息审核结果为{e.__dict__}")
             except QQ_ActionFailed as e:
