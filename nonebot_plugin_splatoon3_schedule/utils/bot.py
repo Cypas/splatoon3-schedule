857bfb5f870b00ce9ecd1289a6c54bd4f4177593
cypas <ayano05@outlook.com>
2026-08-31 10:04:01 +0800

    fix：dc消息类型兼容

--- nonebot_plugin_splatoon3_schedule/utils/bot.py
+++ nonebot_plugin_splatoon3_schedule/utils/bot.py
@@ -65,11 +65,11 @@ from nonebot.adapters.discord import GuildMessageCreateEvent as Dc_GME  # 服务
 
 
 # bot
-All_BOT = (V11_Bot, V12_Bot, Kook_Bot, Tg_Bot, QQ_Bot)
+All_BOT = (V11_Bot, V12_Bot, Kook_Bot, Tg_Bot, QQ_Bot, Dc_Bot)
 # 需要限制qq平台停用的功能也应该是在该功能前直接阻断，而不是后续再进行过滤，故弃用All_BOT_Without_QQ
 
 # 公开发言消息类型
-All_Group_Message = (Kook_CME, Tg_GME, Tg_CME, QQ_CME, QQ_GATME, QQ_GME, V11_GME, V12_GME, V12_CME)
-All_Group_Message_Without_QQ_G = (Kook_CME, Tg_GME, Tg_CME, QQ_CME, V11_GME, V12_GME, V12_CME)
+All_Group_Message = (Kook_CME, Tg_GME, Tg_CME, QQ_CME, QQ_GATME, QQ_GME, V11_GME, V12_GME, V12_CME, Dc_GME)
+All_Group_Message_Without_QQ_G = (Kook_CME, Tg_GME, Tg_CME, QQ_CME, V11_GME, V12_GME, V12_CME, Dc_GME)
 # 私聊消息
-All_Private_Message = (Kook_PME, Tg_PME, QQ_PME, QQ_C2CME, V11_PME, V12_PME)
+All_Private_Message = (Kook_PME, Tg_PME, QQ_PME, QQ_C2CME, V11_PME, V12_PME, Dc_PME)
